fb0d77583139f6ee1221974d0620d2a5a5058631
balaji <vasappagaribalajigowd@gmail.com>
2026-03-10 00:18:50 +0530

    Added the merge_audio_video

--- ai_dubber/app/pipeline.py
+++ ai_dubber/app/pipeline.py
@@ -2,12 +2,13 @@ from .audio_extractor import extract_audio
 from .speech_to_text import audio_to_text
 #from .translator import translate_text
 from .text_to_speech import text_to_speech
+from .merge_audio_video import merge_audio_video
 
 def process_video(video_path):
 
     audio_path = "ai_dubber/data/audio/extracted.wav"
     output_audio = "ai_dubber/data/outputs/english_audio.wav"
-
+    output_video = "ai_dubber/data/outputs/final_video.mp4"
     # Step 1: Extract audio
     extract_audio(video_path, audio_path)
 
@@ -20,5 +21,11 @@ def process_video(video_path):
     
     # Step 4: Convert English text to speech
     text_to_speech(text, output_audio)
+    
+    merge_audio_video(
+    video_path=video_path,
+    audio_path=output_audio,
+    output_path=output_video
+    )
 
-    return output_audio
+    return output_video
