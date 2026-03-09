e5838d63765050252184438c3ba7be1301cad334
balaji <vasappagaribalajigowd@gmail.com>
2026-03-10 00:19:59 +0530

    Changes to main

--- ai_dubber/main.py
+++ ai_dubber/main.py
@@ -3,7 +3,7 @@ from ai_dubber.app.pipeline import process_video
 if __name__ == "__main__":
     print("Main file started")
 
-    video_path = "ai_dubber/data/uploads/video2.mp4"
+    video_path = "ai_dubber/data/uploads/test.mp4"
 
     result = process_video(video_path)
 
